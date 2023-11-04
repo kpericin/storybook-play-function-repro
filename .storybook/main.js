35a6a061a8b4652c02c7162e0dcdc185926fc6c8
Katja Pericin <4922095+kpericin@users.noreply.github.com>
2023-11-04 21:23:30 +0100

    cleanup unused

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  stories: ["../src/**/RhfNativeInteraction.stories.@(js|jsx|ts|tsx)"],
-  // stories: ["../src/**/NativeInteraction.stories.@(js|jsx|ts|tsx)", "../src/**/RhfNativeInteraction.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
